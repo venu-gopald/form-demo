64f2a508552bdf2bbab6b3c8afc49e68167051b8
Venugopal D <venugopald@Vv-Pro.home>
2017-11-24 21:33:04 -0500

    Removed the "welcome to react" title

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-          <h1 className="App-title">Welcome to React</h1>
-        <p className="App-intro">
           <FormComponent/>
-        </p>
       </div>
     );
   }
